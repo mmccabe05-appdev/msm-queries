43fe2a6c7751918d88f0c4c8b252dc26544a77ad
mmccabe05 <mmccabe@chicagobooth.edu>
2022-10-29 21:27:59 +0000

    end of class on saturday, eldest in progress, individual director page not yet functional

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,18 @@ class DirectorsController < ApplicationController
   def index
     render({:template => "director_templates/index.html.erb"})
   end
+
+  def eldest
+    @directors_array = Director.all.order({:dob=>"asc"})
+    @oldest_director_name = Director.all.last
+    render({:template=>"director_templates/eldest.html.erb"})
+  end
+
+  def director_details
+    @director_id = params.fetch(:director_id)
+
+    @director_hash = Director.all.fetch(@director_id)
+
+    render({:template=>"director_templates/director_details.html.erb"})
+  end 
 end
